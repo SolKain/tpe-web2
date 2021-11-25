a37b63adf62757fa0ee2c4804988abad1a1b42ad
Fermin Maly <ferminmalyvarni@gmail.com>
2021-11-24 23:08:32 -0300

    nose

--- RouteApi.php
+++ RouteApi.php
@@ -1,19 +1,19 @@
 <?php
-require_once 'libs/Router.php';
-require_once './controller/apiComentController.php';
-require_once './controller/usuarioController.php';
-$router = new Router();
+    require_once 'libs/Router.php';
+    require_once './controller/apiComentController.php';
+    require_once './controller/usuarioController.php';
+    $router = new Router();
 
-//funciona
-$router->addRoute('comentariosGenerales', 'GET', 'apiComentController', 'getComentariosGenerales');
+    //funciona
+    $router->addRoute('comentariosGenerales', 'GET', 'apiComentController', 'getComentariosGenerales');
 
-//funciona
-$router->addRoute('comentario/:ID', 'POST', 'apiComentController', 'insertComentario');
+    //funciona
+    $router->addRoute('comentario/:ID', 'POST', 'apiComentController', 'insertComentario');
 
-//funciona
-$router->addRoute('comentarios/:ID', 'GET', 'apiComentController', 'getComentarios');
-$router->addRoute('comentario/:ID', 'DELETE', 'apiComentController', 'deleteComentario');
+    //funciona
+    $router->addRoute('comentarios/:ID', 'GET', 'apiComentController', 'getComentarios');
+    $router->addRoute('comentario/:ID', 'DELETE', 'apiComentController', 'deleteComentario');
 
-$router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
+    $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
 
 ?>
